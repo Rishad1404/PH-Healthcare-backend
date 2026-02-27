6c1a1a1d1d58d2f7bc0604a6f541eced8dcf2692
Rishad1404 <rishadislam2019@gmail.com>
2026-02-27 14:22:02 +0600

    server errors handlers

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,69 @@
 
+import { Server } from "http";
 import app from "./app";
 import { seedSuperAdmin } from "./app/utils/seed";
 import { envVars } from "./config/env";
 
+
+let server:Server;
+
 const bootstrap=async()=>{
     try {
         await seedSuperAdmin();
-        app.listen(envVars.PORT,()=>{
+        server=app.listen(envVars.PORT,()=>{
             console.log(`Server is running on http://localhost:${envVars.PORT}`);
         })
     } catch (error) {
         console.log("Failed to start server",error);
     }
 }
+
+// SIGTERM signal handler
+process.on("SIGTERM",()=>{
+    console.log("SIGTERM signal received... Server is shutting down");
+    if(server){
+        server.close(()=>{
+            console.log("Server closed successfully");
+            process.exit(0);
+        })
+    }
+    process.exit(0);
+})
+
+// SIGINT signal handler
+process.on("SIGINT",()=>{
+    console.log("SIGINT signal received... Server is shutting down");
+    if(server){
+        server.close(()=>{
+            console.log("Server closed successfully");
+            process.exit(0);
+        })
+    }
+    process.exit(0);
+})
+
+// uncaught exception handler
+process.on("uncaughtException",error=>{
+    console.log("Uncaught Exception Detected... Server is shutting down",error);
+
+    if(server){
+        server.close(()=>{
+            process.exit(1);
+        })
+    }
+    process.exit(1);
+})
+
+// unhandled rejection handler
+process.on("unhandledRejection",error=>{
+    console.log("Unhandled Rejection Detected... Server is shutting down",error);
+    if(server){
+        server.close(()=>{
+            process.exit(1);
+        })
+    }
+    process.exit(1);
+    
+})
 
 bootstrap()
