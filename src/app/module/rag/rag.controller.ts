7fba3ba15507ec1ef27aae040b7df73763cab5d1
Rishad1404 <rishadislam2019@gmail.com>
2026-04-28 20:08:51 +0600

    query implemented

--- src/app/module/rag/rag.controller.ts
+++ src/app/module/rag/rag.controller.ts
@@ -4,26 +4,47 @@ import { sendResponse } from "../../shared/sendResponse";
 import status from "http-status";
 import { RAGService } from "./rag.service";
 
-const ragService=new RAGService();
+const ragService = new RAGService();
 
-const getStats=async(req:Request,res:Response)=>{
-    console.log("Connected",req.query,res);
-    res.status(200).json({message:"Connected Successfully to RAG"})
-}
+const getStats = async (req: Request, res: Response) => {
+  console.log("Connected", req.query, res);
+  res.status(200).json({ message: "Connected Successfully to RAG" });
+};
 
-const ingestDoctors=catchAsync(async(req:Request,res:Response)=>{
+const ingestDoctors = catchAsync(async (req: Request, res: Response) => {
+  const result = await ragService.ingestDoctorsData();
 
-    const result=await ragService.ingestDoctorsData();
+  sendResponse(res, {
+    httpStatusCode: status.OK,
+    success: true,
+    message: "Doctors ingested successfully",
+    data: result,
+  });
+});
 
-    sendResponse(res,{
-        httpStatusCode:status.OK,
-        success: true,
-        message: "Doctors ingested successfully",
-        data:result
-    })
-})
+const queryRag = catchAsync(async (req: Request, res: Response) => {
 
-export const RagController={
-    getStats,
-    ingestDoctors
-} 
+    const {query,limit,sourceType}=req.body;
+    if(!query){
+        return sendResponse(res,{
+            httpStatusCode: status.BAD_REQUEST,
+            success: false,
+            message: "Query is required",
+        })
+    }
+
+    const result = await ragService.generateAnswer(query,limit??5, sourceType, true);
+
+  sendResponse(res, {
+    httpStatusCode: status.OK,
+    success: true,
+    message: "Answer generated successfully",
+    data: result,
+  });
+});
+
+export const RagController = {
+  getStats,
+  ingestDoctors,
+    queryRag
+};
--- src/app/module/rag/rag.route.ts
+++ src/app/module/rag/rag.route.ts
@@ -4,6 +4,11 @@ import { RagController } from "./rag.controller";
 const router=Router();
 
 router.get("/stats",RagController.getStats);
+
+// index doctors data
 router.post("/ingest-doctors",RagController.ingestDoctors)
+
+// 
+router.post("/query",RagController.queryRag)
 
 export const RagRoutes=router
